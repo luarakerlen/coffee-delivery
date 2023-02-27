fd861501296b33381b6d8709b45c03f6ac5ce9b6
Luara Kerlen dos Santos Rangel <26902816+luarakerlen@users.noreply.github.com>
2023-02-26 21:44:48 -0300

    chore: create global styles

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,21 @@
+export const defaultTheme = {
+  white: '#FFFFFF',
+  background: '#FAFAFA',
+
+  'base-card': '#E1E1E6',
+  'base-input': '#EDEDED',
+  'base-button': '#E6E5E5',
+  'base-hover': '#D7D5D5',
+  'base-label': '#8D8686',
+  'base-text': '#574F4D',
+  'base-subtitle': '#403937',
+  'base-title': '#272221',
+
+  'purple-light': '#EBE5F9',
+  'purple': '#8047F8',
+  'purple-dark': '#4B2995',
+
+  'yellow-light': '#F1E9C9',
+  'yellow': '#DBAC2C',
+  'yellow-dark': '#C47F17',
+}

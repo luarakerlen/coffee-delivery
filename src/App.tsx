862a9721e92ba93ff6e4f129f2bce8fc234f13eb
Luara Kerlen dos Santos Rangel <26902816+luarakerlen@users.noreply.github.com>
2023-02-26 21:59:42 -0300

    feat: create and configure routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
+import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
+import { Router } from './Router';
 import { GlobalStyle } from './styles/global';
 import { defaultTheme } from './styles/themes/default';
 
 export function App() {
 	return (
 		<ThemeProvider theme={defaultTheme}>
-			<h1>Hello World</h1>
+			<BrowserRouter>
+				<Router />
+			</BrowserRouter>
 			<GlobalStyle />
 		</ThemeProvider>
 	);
---
+++ src/pages/Checkout/index.tsx
@@ -0,0 +1,3 @@
+export function Checkout() {
+	return <div>Checkout</div>;
+}

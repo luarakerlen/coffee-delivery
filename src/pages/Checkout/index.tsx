b1aeb0be746a3ef409600ae48e5a8d25bc7994fe
Luara Kerlen dos Santos Rangel <26902816+luarakerlen@users.noreply.github.com>
2023-03-23 23:56:35 -0300

    chore: edit checkout page

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -1,3 +1,43 @@
+import { CurrencyDollar, MapPinLine } from 'phosphor-react';
+import { useTheme } from 'styled-components';
+import { IconTitleDescription } from './components/IconTitleDescription';
+import {
+	AddressContainer,
+	CheckoutContainer,
+	OrderConfirmationContainer,
+	OrderSummaryContainer,
+	PaymentContainer,
+	SelectedCoffeesContainer,
+} from './styles';
+
 export function Checkout() {
-	return <div>Checkout</div>;
+	const theme = useTheme();
+
+	return (
+		<CheckoutContainer>
+			<OrderConfirmationContainer>
+				<p className='containerTitle'>Complete seu pedido</p>
+				<AddressContainer>
+					<IconTitleDescription
+						icon={<MapPinLine size={22} color={theme['yellow-dark']} />}
+						title='Endereço de Entrega'
+						description='Informe o endereço onde deseja receber seu pedido'
+					/>
+				</AddressContainer>
+
+				<PaymentContainer>
+					<IconTitleDescription
+						icon={<CurrencyDollar size={22} color={theme.purple} />}
+						title='Pagamento'
+						description='O pagamento é feito na entrega. Escolha a forma que deseja pagar'
+					/>
+				</PaymentContainer>
+			</OrderConfirmationContainer>
+
+			<SelectedCoffeesContainer>
+				<p className='containerTitle'>Cafés selecionados</p>
+				<OrderSummaryContainer></OrderSummaryContainer>
+			</SelectedCoffeesContainer>
+		</CheckoutContainer>
+	);
 }
--- src/pages/Checkout/styles.ts
+++ src/pages/Checkout/styles.ts
@@ -1,3 +1,43 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
-export const CheckoutContainer = styled.div``
+export const CheckoutContainer = styled.div`
+	display: flex;
+	gap: 2rem;
+	margin: 2.5rem 10rem 0;
+
+	.containerTitle {
+		font-family: 'Baloo 2', sans-serif;
+		font-size: 1.125rem;
+		line-height: 130%;
+		margin-bottom: 0.9375rem;
+	}
+
+	@media (max-width: 768px) {
+		margin: 0 2rem;
+		flex-direction: column;
+	}
+`;
+
+export const OrderConfirmationContainer = styled.div`
+	width: 40rem;
+`;
+
+const DataContainer = styled.div`
+	width: 100%;
+	border-radius: 6px;
+  padding: 2.5rem;
+	background-color: ${(props) => props.theme['base-card']};
+`;
+
+export const AddressContainer = styled(DataContainer)`
+  margin-bottom: 0.75rem;
+`;
+
+export const PaymentContainer = styled(DataContainer)`
+`;
+
+export const SelectedCoffeesContainer = styled.div`
+	width: 28rem;
+`;
+
+export const OrderSummaryContainer = styled.div``;
